b16ff04fa88cb6ebce068c2be3a882a66f38c0a1
SantyVill <santyvillagra@gmail.com>
2023-10-20 15:18:03 -0300

    agrego correcion para bootstrap

--- resources/views/series/edit.blade.php
+++ resources/views/series/edit.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="{{ mix('js/app.js') }}" type="text/javascript"></script> --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <title>Registrar Serie</title>
 </head>
--- resources/views/series/index.blade.php
+++ resources/views/series/index.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <script src="{{ mix('js/app.js') }}" type="text/javascript"></script> --}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <title>Administración de Series</title>
 </head>
